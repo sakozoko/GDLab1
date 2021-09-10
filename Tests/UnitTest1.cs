89d26e64b9888d4dc72f2d4833191208aca90129
sakozoko <olesis.ko@icloud.com>
2021-09-10 20:47:23 +0300

    Created TriangleLib and UnitTests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System.Drawing;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDLab1;
+namespace Tests
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void GetSquareTest()
+        {
+            const double delta = 0.00001d;
+
+            var triangleTest1 = new Triangle(new Point(1, 2), new Point(3, 5), new Point(6, 5));
+            var triangleTest2 = new Triangle(new Point(4, 1), new Point(8, 9), new Point(0, 0));
+            var triangleTest3 = new Triangle(new Point(5, 1), new Point(4, 6), new Point(1, 3));
+            var triangleTest4 = new Triangle(new Point(1, 1), new Point(1, 1), new Point(1, 1));
+            var triangleTest5 = new Triangle(new Point(1, 1), new Point(0, 0), new Point(0, 0));
+
+            const double expected1 = 4.50000d;
+            const double expected2 = 14.00000d;
+            const double expected3 = 9.00000d;
+            const double expected4 = 0.00000d;
+            const double expected5 = 0.00000d;
+
+            var actual1 = triangleTest1.Area;
+            var actual2 = triangleTest2.Area;
+            var actual3 = triangleTest3.Area;
+            var actual4 = triangleTest4.Area;
+            var actual5 = triangleTest5.Area;
+
+            Assert.AreEqual(expected1,actual1,delta);
+            Assert.AreEqual(expected2, actual2, delta);
+            Assert.AreEqual(expected3, actual3, delta);
+            Assert.AreEqual(expected4, actual4, delta);
+            Assert.AreEqual(expected5, actual5, delta);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+
+namespace GDLab1
+{
+    public class Triangle
+    {
+        #region Constructor
+
+        public Triangle(Point point1, Point point2, Point point3)
+        {
+            Line1 = new MyLine(point1, point2);
+            Line2 = new MyLine(point2, point3);
+            Line3 = new MyLine(point3, point1);
+        }
+
+        #endregion
+
+        #region Properties
+
+        private MyLine Line1 { get; }
+        private MyLine Line2 { get; }
+        private MyLine Line3 { get; }
+
+        private double _area;
+        /// <summary>
+        /// Returns area of a triangle, if the triangle is incorrect, returns 0
+        /// </summary>
+        public double Area
+        {
+            get
+            {
+                if (_area is 0d)
+                    CalculateArea();
+                return _area;
+            }
+            private set => _area = value;
+        }
+        #endregion
+
+        #region CalculateAreaMethod
+
+        private void CalculateArea()
+        {
+            var p = (Line1.Length + Line2.Length + Line3.Length) / 2d;
+            Area = Math.Sqrt(p * (p - Line1.Length) * (p - Line2.Length) * (p - Line3.Length));
+        }
+
+        #endregion
+
+        #region Private class MyLine
+
+        private class MyLine
+        {
+
+            #region Constructor
+
+            public MyLine(Point one, Point two)
+            {
+                Vector = new Point(two.X - one.X, two.Y - one.Y);
+                Length = Math.Sqrt(Vector.X * Vector.X + Vector.Y * Vector.Y);
+            }
+
+            #endregion
+
+            #region Properies
+
+            private Point Vector { get; }
+
+            public double Length { get; }
+
+            #endregion
+
+        }
+
+        #endregion
+    }
+}
